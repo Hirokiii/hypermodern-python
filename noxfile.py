87841e48ac537ef19985bbf4e87a381b96ee91d4
hiroki <hiroki.kaminaga@cyber.ee>
2021-04-07 17:28:38 +0300

    cp.4: typing

--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,28 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
 locations = "src", "tests", "noxfile.py"
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "safety", "tests"  # "mypy"
+package = "hypermodern_python"
 
 
 @nox.session(python=["3.8"])
-def black(session):
+def black(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python=["3.8"])
-def lint(session):
+def lint(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(
         session,
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
@@ -27,8 +31,15 @@ def lint(session):
     session.run("flake8", *args)
 
 
+@nox.session(python=["3.8"])
+def mypy(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
 @nox.session(python="3.8")
-def safety(session):
+def safety(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -44,13 +55,21 @@ def safety(session):
 
 
 @nox.session(python=["3.8"])
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args, external=True)
 
 
-def install_with_constraints(session, *args, **kwargs):
+@nox.session(python=["3.8"])
+def typeguard(session: Session) -> None:
+    args = session.posargs or ["-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
+    session.run("pytest", f"--typeguard-packages={package}", *args)
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -18,11 +18,8 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
-    data = wikipedia.random_page(language=language)
+def main(language: str) -> None:
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
